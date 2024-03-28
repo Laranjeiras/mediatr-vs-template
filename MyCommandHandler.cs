ba35167a997658d4701a4b0356aaae63f7074496
Carlos Alexandre Laranjeiras <laranja22@hotmail.com>
2024-03-28 00:05:51 -0300

    First commit

---
+++ MyCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace $rootnamespace$;
+
+public class $servicename$Request : IRequest<$servicename$Response>
+{
+}
+
+public class $servicename$Handler : IRequestHandler<$servicename$Request, $servicename$Response>
+{
+	public $servicename$Handler()
+	{
+	}
+
+	public async Task<$servicename$Response> Handle($servicename$Request request, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+}
